87b3217bb494f1b59e75ed5e28fdec996f61c837
davemeekin <davemeekin.github@gmail.com>
2017-05-18 11:49:08 +0100

    allow push to rubygems, add rubocop and cucumber to travis pipeline

--- lib/scalr_api_v2/version.rb
+++ lib/scalr_api_v2/version.rb
@@ -1,4 +1,4 @@
 # ScalrApiV2::VERSION
 module ScalrApiV2
-  VERSION = '0.1.0'.freeze
+  VERSION = '0.1.1'.freeze
 end
--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe 'ScalrApiV2::VERSION' do
   it 'should set a version number' do
     expect(ScalrApiV2::VERSION).not_to be nil
-    expect(ScalrApiV2::VERSION).to match('0.1.0')
+    expect(ScalrApiV2::VERSION).to match('0.1.1')
   end
 end
