ef7c7c8a4a9668fadc405704069bcb9ad16e32a0
davemeekin <davemeekin.github@gmail.com>
2017-06-09 08:46:15 +0100

    bugfix: duplicate json encoding Scripts class

--- lib/scalr_api_v2/scripts.rb
+++ lib/scalr_api_v2/scripts.rb
@@ -30,31 +30,31 @@ module ScalrApiV2
 
     # Create script accepts script_object as hash and generates JSON before making SCALR api call
     def create(script_object)
-      item = @scalr.create(format('/api/v1beta0/%s/scripts/', @config['mode_path']), JSON.generate(script_object))
+      item = @scalr.create(format('/api/v1beta0/%s/scripts/', @config['mode_path']), script_object)
       return item
     end
 
     # Create script version
     def create_version(script_id, script_object)
-      item = @scalr.create(format('/api/v1beta0/%s/scripts/%s/script-versions/', @config['mode_path'], script_id), JSON.generate(script_object))
+      item = @scalr.create(format('/api/v1beta0/%s/scripts/%s/script-versions/', @config['mode_path'], script_id), script_object)
       return item
     end
 
     # Update script version
     def update_version(script_id, script_version, script_object)
-      item = @scalr.patch(format('/api/v1beta0/%s/scripts/%s/script-versions/%s/', @config['mode_path'], script_id, script_version), JSON.generate(script_object))
+      item = @scalr.patch(format('/api/v1beta0/%s/scripts/%s/script-versions/%s/', @config['mode_path'], script_id, script_version), script_object)
       return item
     end
 
     # execute version
     def execute(script_id, script_execution_object)
-      item = @scalr.post(format('/api/v1beta0/%s/scripts/%s/actions/execute', @config['mode_path'], script_id), JSON.generate(script_execution_object))
+      item = @scalr.post(format('/api/v1beta0/%s/scripts/%s/actions/execute', @config['mode_path'], script_id), script_execution_object)
       return item
     end
 
     # execute script version
     def execute_version(script_id, script_version, script_execution_object)
-      item = @scalr.post(format('/api/v1beta0/%s/scripts/%s/script-versions/%s/actions/execute', @config['mode_path'], script_id, script_version), JSON.generate(script_execution_object))
+      item = @scalr.post(format('/api/v1beta0/%s/scripts/%s/script-versions/%s/actions/execute', @config['mode_path'], script_id, script_version), script_execution_object)
       return item
     end
   end
